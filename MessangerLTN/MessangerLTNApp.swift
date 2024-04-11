89161a989493b5e5252e1bef5705f45592c3f343
mtobi3911 <mtobi3911@gmail.com>
2024-04-11 15:05:37 +0300

    Initial Commit

---
+++ MessangerLTN/MessangerLTNApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessangerLTNApp.swift
+//  MessangerLTN
+//
+//  Created by Никита Курбатов on 11.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MessangerLTNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
